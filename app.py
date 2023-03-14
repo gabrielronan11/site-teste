461005916e61567799984ecd4e5441c17e71e41d
gabrielronan11 <114266038+gabrielronan11@users.noreply.github.com>
2023-03-13 21:50:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é o meu site! (Gabriel Ronan)"
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página sobre"
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página contato"
