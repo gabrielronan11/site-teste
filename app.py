bac6580bb36a4e486e4f90c060f95aacef019f5f
gabrielronan11 <114266038+gabrielronan11@users.noreply.github.com>
2023-03-13 21:15:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-  return "Olá, mundo. Aqui quem fala é o (Gabriel Ronan)"
+  return "Olá, mundo! Esse é o meu site! (Gabriel Ronan)"
